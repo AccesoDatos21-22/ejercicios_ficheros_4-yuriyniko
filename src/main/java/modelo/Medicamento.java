7623c1fa73104f5e3ab0711d4d56460258d3576f
YuriyBTK <yuriybutko.smr@gmail.com>
2021-10-14 13:20:11 +0200

    ejer_1 terminado

--- src/main/java/modelo/Medicamento.java
+++ src/main/java/modelo/Medicamento.java
@@ -1,16 +1,121 @@
 package modelo;
 
-public class Medicamento {
-	
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Medicamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	public final static float IVA = 0.04f;
 	
-	private String nombre; // tamaño 30, 60 bytes
+	public static final int TAM_NOMBRE=40;
+	public static final int TAM_REGISTRO=68;
+
+	private String nombre; // 40 BYTES
 	private double precio; // 8 bytes
 	private int cod; // 4 bytes
 	private int stock; // 4 bytes
 	private int stockMaximo; // 4 bytes
 	private int stockMinimo; // 4 bytes
 	private int codProveedor; // 4 bytes
+	private int contador=1; 
 
+	public Medicamento(String nombre, double precio, int stock, int stockMaximo, int stockMinimo,
+			int codProveedor) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio + (precio * IVA);
+		this.cod = contador;
+		this.stock = stock;
+		this.stockMaximo = stockMaximo;
+		this.stockMinimo = stockMinimo;
+		this.codProveedor = codProveedor;
+		contador++;
+	}
+
+	public Medicamento() {
+		super();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio + (precio * IVA);
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = contador;
+		contador++;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getStockMaximo() {
+		return stockMaximo;
+	}
+
+	public void setStockMaximo(int stockMaximo) {
+		this.stockMaximo = stockMaximo;
+	}
+
+	public int getStockMinimo() {
+		return stockMinimo;
+	}
+
+	public void setStockMinimo(int stockMinimo) {
+		this.stockMinimo = stockMinimo;
+	}
+
+	public int getCodProveedor() {
+		return codProveedor;
+	}
+
+	public void setCodProveedor(int codProveedor) {
+		this.codProveedor = codProveedor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cod);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medicamento other = (Medicamento) obj;
+		return cod == other.cod;
+	}
+
+	@Override
+	public String toString() {
+		return "Codigo:" + cod + "\n Nombre: " + nombre + "\n Precio:" + precio + "â‚¬" + "\n Stock:" + stock
+				+ "\n Proveedor:" + codProveedor;
+	}
 
 }

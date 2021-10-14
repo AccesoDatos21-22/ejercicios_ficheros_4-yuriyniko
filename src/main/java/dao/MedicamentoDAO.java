da74c73be114dc60e8ab8d968c1ed30a474f54e9
YuriyBTK <yuriybutko.smr@gmail.com>
2021-10-14 20:21:44 +0200

    finalizado buscar

--- src/main/java/dao/MedicamentoDAO.java
+++ src/main/java/dao/MedicamentoDAO.java
@@ -7,13 +7,13 @@ import java.util.List;
 import modelo.Medicamento;
 
 public interface MedicamentoDAO {
-	
+
 	// Operaciones CRUD
-	
+
 	public static final String fichero = "medicamentos.bin";
-	
+
 	public static boolean guardar(Medicamento medicamento) {
-		try (RandomAccessFile ficheroM = new RandomAccessFile(fichero,"rw")){
+		try (RandomAccessFile ficheroM = new RandomAccessFile(fichero, "rw")) {
 			ficheroM.seek(medicamento.getCod() * Medicamento.TAM_REGISTRO);
 			StringBuilder nombre = new StringBuilder(medicamento.getNombre());
 			nombre.setLength(Medicamento.TAM_NOMBRE);
@@ -23,21 +23,62 @@ public interface MedicamentoDAO {
 			ficheroM.writeInt(medicamento.getStockMaximo());
 			ficheroM.writeInt(medicamento.getStockMinimo());
 			ficheroM.writeInt(medicamento.getCodProveedor());
-		} catch(IOException exception) {
+		} catch (IOException exception) {
 			return false;
 		}
 		return true;
 	}
-	
-	public Medicamento buscar(String nombre);
+
+	public static Medicamento buscar(String nombre) {
+
+		Medicamento md = new Medicamento();
+
+		try (RandomAccessFile ficheroM = new RandomAccessFile(fichero, "r")) {
+
+			double precio;
+			int Stock, stockMax, stockMin, codProveedor, cod;
+			char nom[] = new char[Medicamento.TAM_NOMBRE], aux;
+
+			do {
+
+				cod = ficheroM.readInt();
+
+				for (int i = 0; i < Medicamento.TAM_NOMBRE; i++) {
+					aux = ficheroM.readChar();
+					if ((int) aux != 0) {
+						nom[i] = aux;
+					} else {
+						nom[i] = ' ';
+					}
+				}
+
+				String nombreM = new String(nom);
+
+				md.setNombre(nombreM);
+				md.setPrecio(ficheroM.readDouble());
+				md.setStock(ficheroM.readInt());
+				md.setStockMaximo(ficheroM.readInt());
+				md.setStockMinimo(ficheroM.readInt());
+				md.setCodProveedor(ficheroM.readInt());
+				md.setCod(ficheroM.readInt());
+
+				System.out.println(md.toString());
+
+			} while (ficheroM.getFilePointer() <= ficheroM.length());
+
+		} catch (Exception e) {
+
+			e.getMessage();
+
+		}
+
+		return md;
+	}
 
 	public boolean actualizar(Medicamento medicamento);
-	
-	public boolean borrar(Medicamento medicamento);	
-	
+
+	public boolean borrar(Medicamento medicamento);
+
 	public List<Medicamento> leerTodos();
-
-
-
 
 }
